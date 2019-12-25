cdcd5e386bd9fe0667ac9635a829889651113de0
System Administrator <nntd290897@gmail.com>
2019-12-25 22:12:49 +0700

    Updates

---
+++ precache-manifest.8a5b01ef97078180ff5cd0ee8c49ab78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "692c2e54ba4b937b87fa9aab7afad2e6",
+    "url": "/MaReservation/index.html"
+  },
+  {
+    "revision": "e0f25ec50e492ff37d3c",
+    "url": "/MaReservation/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5a3e31a86ab2aa098af1",
+    "url": "/MaReservation/static/js/2.586e01c6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/MaReservation/static/js/2.586e01c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e0f25ec50e492ff37d3c",
+    "url": "/MaReservation/static/js/main.6369e2e6.chunk.js"
+  },
+  {
+    "revision": "71cf21de84b917881d6e",
+    "url": "/MaReservation/static/js/runtime-main.2615405d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MaReservation/static/media/logo.5d5d9eef.svg"
+  }
+]);
